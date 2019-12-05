c707fc719f6b2889ab47726791b4cea2aad6c52e
Vladislav <JDinald@gmail.com>
2019-12-05 09:45:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 ITERATION_LIMIT = 1000
 
@@ -12,25 +13,28 @@ print(matrixAB)
 print()
 
 """"Iteration loops"""
+start=time.process_time() 
 x = np.zeros_like(B)
 for iter_count in range(ITERATION_LIMIT):
-    print("X vector: ", x, "iteration: ", iter_count)
-    x_new = np.zeros_like(x)
+  print("X vector: ", x, "iteration: ", iter_count)
+  x_new = np.zeros_like(x)
 
-    for i in range(A.shape[0]):
-        alpha1 = np.dot(A[i, :i], x[:i])
-        alpha2 = np.dot(A[i, i + 1:], x[i + 1:])
-        x_new[i] = (B[i] - alpha1 - alpha2) / A[i, i]
+  for i in range(A.shape[0]):
+    alpha1 = np.dot(A[i, :i], x[:i])
+    alpha2 = np.dot(A[i, i + 1:], x[i + 1:])
+    x_new[i] = (B[i] - alpha1 - alpha2) / A[i, i]
 
-    if np.allclose(x, x_new, atol=1e-10, rtol=0.):
-        break
+  if np.allclose(x, x_new, atol=1e-10, rtol=0.):
+    stop = time.process_time()
+    break
 
-    x = x_new
+  x = x_new
 
 """"Prints solutions"""
 print()
 print("Solution: ")
 print("Vector X: ", x, "Total iterations: ", iter_count)
+print("Elapsed time during the whole function in seconds:", stop-start)
 
 """"Error check"""
 error = np.dot(A, x) - B
